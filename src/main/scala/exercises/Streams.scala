6d9d7328f1afda1f0a3b1e9708eb270ce5a1bb5e
CeciliaTeodorani <cecilia.teodorani@studio.unibo.it>
2022-03-17 12:09:48 +0100

    Add new functions and tests to Stream

---
+++ src/main/scala/exercises/Streams.scala
@@ -0,0 +1,53 @@
+package exercises
+
+import scala.annotation.tailrec
+
+object Streams extends App:
+
+  import Lists.*
+
+  enum Stream[A]:
+    private case Empty()
+    private case Cons(head: () => A, tail: () => Stream[A])
+
+  object Stream:
+
+    def empty[A](): Stream[A] = Empty()
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+
+    def toList[A](stream: Stream[A]): List[A] = stream match
+      case Cons(h, t) => List.Cons(h(), toList(t()))
+      case _ => List.Nil()
+
+    def map[A, B](stream: Stream[A])(f: A => B): Stream[B] = stream match
+      case Cons(head, tail) => cons(f(head()), map(tail())(f))
+      case _ => Empty()
+
+    def filter[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), filter(tail())(pred))
+      case Cons(head, tail) => filter(tail())(pred)
+      case _ => Empty()
+
+    def take[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Cons(head, tail), n) if n > 0 => cons(head(), take(tail())(n - 1))
+      case _ => Empty()
+
+    def iterate[A](init: => A)(next: A => A): Stream[A] = cons(init, iterate(next(init))(next))
+
+    @tailrec
+    def drop[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Cons(head, tail), n) if n > 0 => drop(tail())(n - 1)
+      case (Cons(head, tail), n) => cons(head(), tail())
+      case _ => Empty()
+
+    def constant[A](k: A): Stream[A] = cons(k, constant(k))
+
+    def fibonacci(): Stream[Int] =
+      def _fib(current: Int, next: Int): Stream[Int] = cons(current, _fib(next, current + next))
+      _fib(0, 1)
+
+  end Stream
